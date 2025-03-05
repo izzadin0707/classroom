83d95178fcb0ab76fdadb4a0628351c5f6261d61
Dinzadin <153895119+Dinzadin@users.noreply.github.com>
2025-03-06 01:12:39 +0700

    update routes web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClassController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -19,9 +20,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Classroom
+    Route::get('/classroom', [ClassController::class, 'edit'])->name('classroom');
 });
 
 require __DIR__.'/auth.php';
